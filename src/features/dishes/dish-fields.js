22a62db9fe0d3efe3d6c6fb041edd8e0780385b4
jpilch <pilch.jan.kontakt@gmail.com>
2022-11-16 11:31:37 +0100

    add dish specific form fields

---
+++ src/features/dishes/dish-fields.js
@@ -0,0 +1,43 @@
+import v from "./field-validators";
+import c from "./field-components";
+
+import { Field } from "redux-form";
+
+const pizzaFields = (
+    <>
+        <Field name="no_of_slices" type="number"
+            component={c.renderInputField} label="No. of slices"
+            validate={[v.required, v.int]}
+        />
+        <Field
+            name="diameter" type="number"
+            component={c.renderInputField} label="Diameter"
+            validate={[v.required, v.float]}
+            step={0.01}
+        />
+    </>
+);
+
+const soupFields = (
+    <>
+        <Field name="spiceness_scale" type="number"
+            component={c.renderInputField} label="Spiciness scale"
+            validate={[v.int, v.onSpiceScale]}
+        />
+    </>
+)
+
+const sandwichFields = (
+    <>
+        <Field name="slices_of_bread" type="number"
+            component={c.renderInputField} label="Slices of bread"
+            validate={v.int}
+        />
+    </>
+)
+
+export default new Map([
+    ['pizza', pizzaFields],
+    ['soup', soupFields],
+    ['sandwich', sandwichFields]
+])
