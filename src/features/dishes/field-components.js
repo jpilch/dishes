5531bfbcb7ee6510a70903a51d907b1677cc15fa
jpilch <pilch.jan.kontakt@gmail.com>
2022-11-16 15:07:49 +0100

    style input fields and validation errors

--- src/features/dishes/field-components.js
+++ src/features/dishes/field-components.js
@@ -1,32 +1,63 @@
+import cn from "classnames";
+
+import styles from "../../styles/Form.module.scss";
+
 const renderInputField = ({ input, label, type, step, meta: { touched, error } }) => {
+    console.log({ styles });
     return (
-        <div >
-            <label >{label}</label>
+        <div className={styles.form__field}>
+            <label className={styles.form__label}>
+                {label}
+            </label>
             <input
+                className={cn({
+                    [styles.form__input]: true,
+                    [styles["form__input--error"]]: touched && error,
+                })}
                 {...input}
                 step={step}
                 type={type}
             />
-            {touched && error && <span >{error}</span>}
+            {touched && error && (
+                <span className={styles.form__error}>
+                    {error}
+                </span>
+            )}
         </div >
     )
 }
 
 const renderSelectField = ({ input, label, type, meta: { touched, error } }) => {
     return (
-        <div  >
-            <label >{label}</label>
+        <div className={styles.form__field}>
+            <label className={styles.form__label}>
+                {label}
+            </label>
             <select
+                className={cn({
+                    [styles.form__select]: true,
+                    [styles["form__select--error"]]: touched && error,
+                })}
                 {...input}
                 placeholder={label}
                 type={type}
             >
                 <option></option>
-                <option value="pizza">Pizza</option>
-                <option value="soup">Soup</option>
-                <option value="sandwich">Sandwich</option>
+                <option value="pizza">
+                    Pizza
+                </option>
+                <option value="soup">
+                    Soup
+                </option>
+                <option value="sandwich">
+                    Sandwich
+                </option>
             </select >
-            {touched && error && <span >{error}</span>}
+            {touched && error && (
+                <span className={styles.form__error}>
+                    {error}
+                </span>
+            )}
         </div >
     );
 }
