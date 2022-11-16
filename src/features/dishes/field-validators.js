5af9b0fbbae4da04559b2ad8dd95e194e1cbe0b2
jpilch <pilch.jan.kontakt@gmail.com>
2022-11-16 11:27:04 +0100

    add form field validators

---
+++ src/features/dishes/field-validators.js
@@ -0,0 +1,39 @@
+function required(value) { return !value ? 'Required' : undefined; }
+
+function int(value) {
+    return value && isNaN(parseInt(value))
+        ? 'Must be a number'
+        : undefined
+}
+
+function intInRange(from, to) {
+    return function (value) {
+        return int(value) || (value < from || value > to)
+            ? `Must be in range ${from} - ${to}`
+            : undefined;
+    }
+}
+
+function float(value) {
+    return value && isNaN(parseFloat(value))
+        ? "Must be a floating point number"
+        : undefined;
+}
+
+function onSpiceScale(value) {
+    return intInRange(1, 10)(value);
+}
+
+function inTimeFormat(text) {
+    return text.search(/\b((0[1-9])|(1[0-9])|(2[0-4])):[0-5][0-9]:[0-5][0-9]\b/) === -1
+        ? "Must be in format HH:MM:SS"
+        : undefined;
+}
+
+export default {
+    required,
+    int,
+    float,
+    onSpiceScale,
+    inTimeFormat
+};
